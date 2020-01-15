484aca297a29745dfd29a6336a8682152bdc6f1e
SavLM <50849597+SavLM@users.noreply.github.com>
2020-01-15 00:17:31 -0500

    working on palindrome thing

--- EulerSolutions.h
+++ EulerSolutions.h
@@ -2,4 +2,7 @@
 class EulerSolution {
 public:
 	int MultiplesOf3And5(int input);  // implemented by subclass
+	int EvenFibonacciNumbers(int input);
+	long long LargestPrimeFactor(long long input);
+	int LargestPalindromeProduct(int input);
 };
--- EulerTest.cpp
+++ EulerTest.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "EulerSolutions.h"
-#include <vector>
+#include <Windows.h>
+#include <string>
 
 int main()
 {
@@ -8,17 +9,50 @@ int main()
 	EulerSolution doit;
 	int numProbs = 0;
 	int correctSols = 0;
-	
-	int ret = doit.MultiplesOf3And5(10);
-	if (ret == 23) correctSols++;
-	else printf("Problem %i Failed\n Output: %i\n", numProbs, ret);
+	int ret = 0;
+	/*
+	// 1
 	numProbs++;
-
+	ret = doit.MultiplesOf3And5(10);
+	if (ret == 23) correctSols++;
+	else printf("Problem %i Failed\nOutput: %i\n", numProbs, ret);
+	
+	// 2
+	numProbs++;
 	ret = doit.MultiplesOf3And5(1000);
 	if (ret == 233168) correctSols++;
-	else printf("Problem %i Failed\n Output: %i\n", numProbs, ret);
+	else printf("Problem %i Failed\nOutput: %i\n", numProbs, ret);
+	
+	// 3
 	numProbs++;
+	ret = doit.EvenFibonacciNumbers(4000000);
+	if (ret == 4613732) correctSols++;
+	else printf("Problem %i Failed\nOutput: %i\n", numProbs, ret);
 
+	// 4
+	numProbs++;
+	long long uret = doit.LargestPrimeFactor(13195);
+	if (uret == 29) correctSols++;
+	else printf("Problem %i Failed\nOutput: %lld\n", numProbs, uret);
+	
+	// 5
+	numProbs++;
+	uret = doit.LargestPrimeFactor(600851475143);
+	if (uret == 6857) correctSols++;
+	else printf("Problem %i Failed\nOutput: %lld\n", numProbs, uret);
+	*/
+	// 6
+	numProbs++;
+	ret = doit.LargestPalindromeProduct(8000);
+	if (ret == 6857) correctSols++;
+	//else printf("Problem %i Failed\nOutput: %i\n", numProbs, ret);
+	if (numProbs == correctSols) printf("All tests passed\n");
+	else printf("There were %i failures.\n", numProbs - correctSols);
+
+	numProbs++;
+	ret = doit.LargestPalindromeProduct(1);
+	if (ret == 6857) correctSols++;
+	//else printf("Problem %i Failed\nOutput: %i\n", numProbs, ret);
 	if (numProbs == correctSols) printf("All tests passed\n");
 	else printf("There were %i failures.\n", numProbs - correctSols);
 
